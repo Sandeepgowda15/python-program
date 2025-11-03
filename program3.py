38199e78cbfdd7d3d1643897716de6377c0ae901
sandeep <sandeepgowda.sg10@gmail.com>
2025-11-03 17:16:49 +0530

    second commit

---
+++ program3.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python3
+
+x = 60
+y = 70
+z = 90
+average = (x + y + z) / 3
+print("Average:", average)
---
+++ program4.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+
+base = 10
+height = 6
+area = 0.5 * base * height
+print("Area of triangle:",area)
